e0808b4afd1f626d4e9cb66d0a2a7a3c382287c2
Respatyadi <respatyaditu@gmail.com>
2023-02-04 22:58:22 +0700

    commit

--- migrations/20230204151509-addPrimaryKey.js
+++ migrations/20230204151509-addPrimaryKey.js
@@ -9,6 +9,77 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.addColumn("Users", "RoleId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Roles",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+    
+    await queryInterface.addColumn("Roles", "UserId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await queryInterface.addColumn("Products", "UserId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await queryInterface.addColumn("Orders", "UserId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Users",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await queryInterface.addColumn("Orders", "ItemId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Items",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+
+    await queryInterface.addColumn("Items", "ProductId", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Products",
+        key: "id",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
+    await queryInterface.addColumn("Items", "priceFK", {
+      type: Sequelize.INTEGER,
+      references: {
+        model: "Products",
+        key: "price",
+      },
+      onUpdate: "cascade",
+      onDelete: "cascade",
+    });
+
   },
 
   async down (queryInterface, Sequelize) {
@@ -18,5 +89,12 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.removeColumn("Users", "RoleId", {}),
+    await queryInterface.removeColumn("Roles", "UserId", {}),
+    await queryInterface.removeColumn("Products", "UserId", {}),
+    await queryInterface.removeColumn("Orders", "UserId", {}),
+    await queryInterface.removeColumn("Orders", "ItemId", {}),
+    await queryInterface.removeColumn("Items", "ProductId", {})
+    await queryInterface.removeColumn("Items", "priceFK", {})
   }
 };
